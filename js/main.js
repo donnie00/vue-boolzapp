7b1d59d4033b7b4f492c8d34bdd82061036ab746
ippoRulez <ippolitodonato@gmail.com>
2022-11-25 20:14:51 +0100

    added message input

--- js/main.js
+++ js/main.js
@@ -7,6 +7,12 @@ createApp({
 		return {
 			contacts,
 			activeContactIndex: 0,
+			messageSent: '',
+			newMessage: {
+				date: '',
+				message: '',
+				status: 'sent',
+			},
 		};
 	},
 	//inserisci qui le tue funzioni
@@ -14,5 +20,18 @@ createApp({
 		changeActiveChat(index) {
 			this.activeContactIndex = index;
 		},
+		sendNewMessage() {
+			const sendingTime = luxon.DateTime.now();
+			const sendingHour = sendingTime.hour;
+			const sendingMinute = sendingTime.minute;
+
+			this.newMessage.message = this.messageSent;
+			this.newMessage.date = `${sendingHour}:${sendingMinute}`;
+			this.contacts[this.activeContactIndex].messages.push(this.newMessage);
+
+			this.messageSent = '';
+		},
+
+		sendOkAnswer() {},
 	},
 }).mount('#app');
